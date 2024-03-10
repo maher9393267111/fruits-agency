cb6b737be4ce892f60821de287aade4473229426
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-03-11 00:10:39 +0300

    add

--- pages/admin/product/edit/[id].js
+++ pages/admin/product/edit/[id].js
@@ -1,80 +1,55 @@
-import React ,{useState ,useEffect} from 'react';
-import UpdateProduct from '../../../../src/components/admin/product/updateProduct';
-import { getDocuments,getDocument } from '../../../../src/functions/firebase/getData';
-import Loader from '../../../../src/components/admin/common/Loader';
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import UpdateProduct from "../../../../src/components/admin/product/updateProduct";
+import {
+  getDocuments,
+  getDocument,
+} from "../../../../src/functions/firebase/getData";
+import Loader from "../../../../src/components/admin/common/Loader";
 
+const EditSubPage = ({  }) => {
+  //const product = await getDocument("products", context.query.id);
 
-const EditSubPage = ({id}) => {
+  const [product, setProduct] = useState({});
+  const [loacding, setLoading] = useState(true);
 
-    //const product = await getDocument("products", context.query.id);
+  const router = useRouter();
+  const id = router.query.id ;
 
-    const [product ,setProduct] = useState({})
-const [loacding, setLoading] = useState(true)
+  // const Products = await getDocuments("products")
 
-// const Products = await getDocuments("products")
+  useEffect(() => {
+    const getProducts = async () => {
+      setLoading(true);
+      setProduct({});
+      const data = await getDocument("products", id);
+      console.log(data, "fetch categories ====>>>>");
+      setProduct(data);
+      setLoading(false);
+    };
 
+    if (id) getProducts();
+  }, []);
 
-
-useEffect(() => {
-  const getProducts = async () => {
-
-    setLoading(true)
-      setProduct({})
-      const data   = await getDocument("products",id);
-      console.log(data,"fetch categories ====>>>>")
-      setProduct(data)
-      setLoading(false)
-    }
-
-    if(id) getProducts();
-}, []);
-
-
-    return (
-        <div>
-
-{loacding ? 
-<Loader/>
-:
-    
-<UpdateProduct
-product={product}
-
-/>
-
-}
-
-
-
-        </div>
-    );
-}
-
+  return (
+    <div>{loacding ? <Loader /> : <UpdateProduct product={product} />}</div>
+  );
+};
 
 export default EditSubPage;
 
-
-
 // serverside to fetch single catgory in serverside from firestore
 
+// EditSubPage.getInitialProps = async (context) => {
 
+//     const id =context.query.id
 
+//     // context.query.id ==> admin/category/edit/${context.query.id} in browser
+//       //  const product = await getDocument("products", context.query.id);
 
-EditSubPage.getInitialProps = async (context) => {
- 
-    const id =context.query.id
+//         return {
+//             id:id
+//            // product: product,
 
-
-    // context.query.id ==> admin/category/edit/${context.query.id} in browser
-      //  const product = await getDocument("products", context.query.id);
-       
-     
-    
-    
-     
-        return {
-            id:id
-           // product: product,
-          
-        };
-      };
+//         };
+//       };
