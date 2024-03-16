5ffe57917f64b51df005fd98906438d854e5e764
Zaher Zeinni <zaherzeinni@gmail.com>
2024-03-16 21:58:35 +0200

    ok

--- pages/home.jsx
+++ pages/home.jsx
@@ -14,9 +14,6 @@ import AboutSectionHome from "components/ProjectComponents/AboutSectionHome";
 
 import api from "utils/__api__/grocery3-shop";
 
-
-import About from "./about";
-
 export default function Home(props) {
   console.log("Products");
 
@@ -63,14 +60,17 @@ export default function Home(props) {
         <Loader />
       ) : (
         <div className="mb-12">
-          <HomeSlider products={products} mainCarouselData={props.mainCarouselData} />
+          <HomeSlider
+            products={products}
+            mainCarouselData={props.mainCarouselData}
+          />
 
           <Container
             sx={{
               mb: 6,
             }}
           >
-            <HomeOffer  offers={props.offerCards} />
+            <HomeOffer offers={props.offerCards} />
 
             <HomeProductsSlider
               isorderpage={false}
@@ -115,3 +115,100 @@ export const getStaticProps = async ({ locale }) => {
     },
   };
 };
+
+import React, { useState, useEffect } from "react";
+import { Container , Box } from "@mui/material";
+import MainLayout from "../src/components/ProjectComponents/mainLayout";
+import HomeSlider from "components/ProjectComponents/HomeSlider";
+import HomeOffer from "components/ProjectComponents/HomeOffer";
+import HomeProductsSlider from "components/ProjectComponents/HomeProductsSlider";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import { orderBy, where, collection, query, getDocs } from "firebase/firestore";
+import { db } from "functions/firebase";
+import { getDocuments, getDocumentsOrder } from "functions/firebase/getData";
+import Loader from "components/admin/common/Loader";
+import AboutSectionHome from 'components/ProjectComponents/AboutSectionHome'
+
+import api from "utils/__api__/grocery3-shop";
+
+
+import About from "./about";
+
+export default function Home(props) {
+  console.log("Products");
+
+  const [products, setProducts] = useState([]);
+  const [loacding, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      setLoading(true);
+      setProducts([]);
+      const data = await getDocumentsOrder(
+        "products",
+        orderBy("timeStamp", "asc"),
+         where("ismedia", "==", false)
+      );
+
+      console.log(data, "fetch products ====>>>>");
+      setProducts(data);
+      setLoading(false);
+    };
+    getProducts();
+  }, []);
+
+
+
+
+  return (
+    <MainLayout>
+      
+      {loacding ? (
+        <Loader />
+      ) : (
+        <div className="mb-12">
+          {/* mainCarouselData={props.mainCarouselData} */}
+          <HomeSlider products={products}  />
+
+          <Container
+            sx={{
+              mb: 6,
+            }}
+          >
+            {/* <About /> */}
+            <HomeOffer  offers={props.offerCards} />
+            
+            <HomeProductsSlider
+            
+              isorderpage={false}
+              products= {products}
+              // {props.topSailedProducts}
+            />
+
+
+
+<AboutSectionHome/>
+
+          </Container>
+        </div>
+      )}
+    </MainLayout>
+  );
+}
+
+export const getStaticProps = async ({ locale }) => {
+  const mainCarouselData = await api.getMainCarousel();
+  const offerCards = await api.getOfferCards();
+  //const topSailedProducts = await api.getTopSailedProducts();
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+
+      mainCarouselData,
+      offerCards,
+    //  topSailedProducts,
+    },
+  };
+};
+
